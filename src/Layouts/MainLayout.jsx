1abfa0082b7ba3b2ce4920cd10df66a3fa5fdf67
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-09-04 08:29:51 -0700

    routes layouts and folder setup

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <h1>Layout</h1>
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default MainLayout;
