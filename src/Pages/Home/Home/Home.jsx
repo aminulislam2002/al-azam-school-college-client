1e252fdc1499217e8b608102c853c3c9e44a05fe
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-09-06 12:39:43 -0700

    add madrasah photo gallery

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import ContactInformation from "../ContactInformation/ContactInformation";
 import LocationAndAddress from "../LocationAndAddress/LocationAndAddress";
 import MadrasahInfoAndNotice from "../MadrasahInfoAndNotice/MadrasahInfoAndNotice";
 import MadrasahInformation from "../MadrasahInformation/MadrasahInformation";
+import MadrasahPhotoGallery from "../MadrasahPhotoGallery/MadrasahPhotoGallery";
 import Statistics from "../Statistics/Statistics";
 import TeachersSection from "../TeachersSection/TeachersSection";
 
@@ -14,6 +15,7 @@ const Home = () => {
       <TeachersSection></TeachersSection>
       <MadrasahInfoAndNotice></MadrasahInfoAndNotice>
       <Statistics></Statistics>
+      <MadrasahPhotoGallery></MadrasahPhotoGallery>
       <ContactInformation></ContactInformation>
       <LocationAndAddress></LocationAndAddress>
       <h1>Home</h1>
