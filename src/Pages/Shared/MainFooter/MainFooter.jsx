9c26ca284291647dae6da1f4ee24f851ed72a7a8
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-09-06 14:06:19 -0700

    update footer section

--- src/Pages/Shared/MainFooter/MainFooter.jsx
+++ src/Pages/Shared/MainFooter/MainFooter.jsx
@@ -4,17 +4,19 @@ import { AiFillInstagram, AiFillTwitterCircle, AiFillYoutube } from "react-icons
 import { Link } from "react-router-dom";
 
 const MainFooter = () => {
+  const logo =
+    "https://img.freepik.com/free-vector/quran-design-ramadan-kareem_1394-247.jpg?size=626&ext=jpg&ga=GA1.1.235952763.1681062999&semt=ais";
   return (
     <div>
       <footer className="footer p-10 bg-gray-900 text-white ">
-        <div>
-          <img className="w-20 h-20" src="" alt="" />
-          <p>
-            Name of Madrasah
-            <br />
-            Details of Madrasah
-          </p>
+        <div className="flex items-center">
+          <img className="w-20 h-20 rounded-full border-2 border-blue-500" src={logo} alt="Madrasah Logo" />
+          <div className="ml-4">
+            <p className="text-lg font-semibold text-white">Name of Madrasah</p>
+            <p className="text-gray-400">Details of Madrasah</p>
+          </div>
         </div>
+
         <div>
           <span className="footer-title">Services</span>
           <a className="link link-hover">Branding</a>
@@ -37,23 +39,23 @@ const MainFooter = () => {
         </div>
       </footer>
 
-      <footer className="footer items-center p-4 bg-neutral text-neutral-content">
-        <div className="items-center grid-flow-col">
-          <p>Copyright © 2023 - All right reserved</p>
-        </div>
-        <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-          <Link>
-            <MdOutlineFacebook className="w-10 h-10"></MdOutlineFacebook>
-          </Link>
-          <Link>
-            <AiFillTwitterCircle className="w-10 h-10"></AiFillTwitterCircle>
-          </Link>
-          <Link>
-            <AiFillInstagram className="w-10 h-10"></AiFillInstagram>
-          </Link>
-          <Link>
-            <AiFillYoutube className="w-10 h-10"></AiFillYoutube>
-          </Link>
+      <footer className="bg-neutral text-neutral-content py-4">
+        <div className="container mx-auto lg:px-52 flex justify-between items-center">
+          <p className="text-sm">Copyright © 2023 - All rights reserved</p>
+          <div className="flex gap-4">
+            <Link className="">
+              <MdOutlineFacebook className="w-5 lg:w-10 h-5 lg:h-10 text-blue-500 hover:text-blue-700 transition-colors duration-300" />
+            </Link>
+            <Link className="">
+              <AiFillTwitterCircle className="w-5 lg:w-10 h-5 lg:h-10 text-blue-400 hover:text-blue-600 transition-colors duration-300" />
+            </Link>
+            <Link className="">
+              <AiFillInstagram className="w-5 lg:w-10 h-5 lg:h-10 text-pink-500 hover:text-pink-700 transition-colors duration-300" />
+            </Link>
+            <Link className="">
+              <AiFillYoutube className="w-5 lg:w-10 h-5 lg:h-10 text-red-600 hover:text-red-800 transition-colors duration-300" />
+            </Link>
+          </div>
         </div>
       </footer>
     </div>
