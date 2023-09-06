4172dcc09be3e2021a8c2f821b82e743931ab678
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-09-06 06:43:07 -0700

    add simple madrasah statistics

--- src/Pages/Home/TeachersSection/TeachersSection.jsx
+++ src/Pages/Home/TeachersSection/TeachersSection.jsx
@@ -15,7 +15,7 @@ const TeachersSection = () => {
   }, []);
 
   return (
-    <div className="bg-gray-100 py-12">
+    <div className="py-12">
       <div className="container mx-auto px-4">
         <h1 className="text-3xl font-semibold text-center mb-6">Meet our Teachers</h1>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
