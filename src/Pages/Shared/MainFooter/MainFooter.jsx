63933da89c6aabf7c5000c3625b8bd3044d9163a
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-09-07 14:10:38 -0700

    simple disign footer and responsive

--- src/Pages/Shared/MainFooter/MainFooter.jsx
+++ src/Pages/Shared/MainFooter/MainFooter.jsx
@@ -1,62 +1,170 @@
-import { MdOutlineFacebook } from "react-icons/md";
+import { MdEmail, MdLocationOn, MdOutlineFacebook } from "react-icons/md";
 import { AiFillInstagram, AiFillTwitterCircle, AiFillYoutube } from "react-icons/ai";
-
 import { Link } from "react-router-dom";
+import logo from "../../../assets/image/logo.png";
+import { IoMdCall } from "react-icons/io";
 
 const MainFooter = () => {
-  const logo =
-    "https://img.freepik.com/free-vector/quran-design-ramadan-kareem_1394-247.jpg?size=626&ext=jpg&ga=GA1.1.235952763.1681062999&semt=ais";
   return (
     <div>
-      <footer className="footer p-10 bg-gray-900 text-white ">
-        <div className="flex items-center">
-          <img className="w-20 h-20 rounded-full border-2 border-blue-500" src={logo} alt="Madrasah Logo" />
-          <div className="ml-4">
-            <p className="text-lg font-semibold text-white">Name of Madrasah</p>
-            <p className="text-gray-400">Details of Madrasah</p>
+      {/* Footer for Large Screens */}
+      <footer className="text-black lg:pt-20 pb-10 bg-gray-300 hidden lg:block">
+        <div className="grid grid-cols-12">
+          {/* Left Section */}
+          <div className="col-span-3 p-6">
+            <img className="w-20 h-20 rounded-full border-2 border-blue-500 mx-auto" src={logo} alt="Madrasah Logo" />
+            <p className="text-lg font-semibold text-green-500 text-center mt-4">Al-Azam High School and College</p>
+            <div className="mt-2">
+              <IoMdCall className="w-4 h-4 text-green-500 inline-block"></IoMdCall>{" "}
+              <span className="text-sm lg:text-base">01715-356338</span>
+            </div>
+            <div className="mt-2">
+              <MdEmail className="w-4 h-4 text-green-500 inline-block"></MdEmail>
+              <span className="lg:text-sm text-base">alazamhighschoolandcollege@gmail.com</span>
+            </div>
+            <div className="mt-2">
+              <MdLocationOn className="w-4 h-4 text-green-500 inline-block"></MdLocationOn>
+              <span className="lg:text-sm text-base">Amtoli, Bishwanath. Sylhet, Bangladesh</span>
+            </div>
           </div>
-        </div>
 
-        <div>
-          <span className="footer-title">Services</span>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
-        </div>
-        <div>
-          <span className="footer-title">Company</span>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
-        </div>
-        <div>
-          <span className="footer-title">Legal</span>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+          {/* Middle Section */}
+          <div className="col-span-5 p-6">
+            <span className="footer-title text-xl block mb-4">Quick Links</span>
+            <div className="flex justify-around items-start">
+              <div className="mt-2">
+                <a className="link link-hover block text-base py-1">DSHE</a>
+                <a className="link link-hover block text-base py-1">BANBEIS</a>
+                <a className="link link-hover block text-base py-1">BD National Portal</a>
+                <a className="link link-hover block text-base py-1">Ministry of Education</a>
+              </div>
+              <div className="mt-2">
+                <a className="link link-hover block text-base py-1">Sylhet Board</a>
+                <a className="link link-hover block text-base py-1">Primary & Mass Education</a>
+                <a className="link link-hover block text-base py-1">Form of BD. Govt</a>
+                <a className="link link-hover block text-base py-1">Pathshala EIMS</a>
+              </div>
+            </div>
+          </div>
+
+          {/* Right Section */}
+          <div className="col-span-2 p-6">
+            <span className="footer-title text-xl block mb-4">Legal</span>
+            <div className="mt-2">
+              <a className="link link-hover block text-base py-1">Terms of Use</a>
+              <a className="link link-hover block text-base py-1">Privacy Policy</a>
+              <a className="link link-hover block text-base py-1">Cookie Policy</a>
+            </div>
+          </div>
+
+          {/* Social Media Section */}
+          <div className="col-span-2 p-6">
+            <span className="footer-title text-xl block mb-4">Follow us</span>
+            <div className="mt-2 flex justify-center items-start gap-4">
+              <div>
+                <Link to="#" className="text-blue-500 hover:text-blue-700 transition-colors duration-300">
+                  <MdOutlineFacebook className="w-6 h-6 lg:w-8 lg:h-8" />
+                </Link>
+              </div>
+              <div>
+                <Link to="#" className="text-blue-400 hover:text-blue-600 transition-colors duration-300">
+                  <AiFillTwitterCircle className="w-6 h-6 lg:w-8 lg:h-8" />
+                </Link>
+              </div>
+              <div>
+                <Link to="#" className="text-pink-500 hover:text-pink-700 transition-colors duration-300">
+                  <AiFillInstagram className="w-6 h-6 lg:w-8 lg:h-8" />
+                </Link>
+              </div>
+              <div>
+                <Link to="#" className="text-red-600 hover:text-red-800 transition-colors duration-300">
+                  <AiFillYoutube className="w-6 h-6 lg:w-8 lg:h-8" />
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </footer>
 
-      <footer className="bg-neutral text-neutral-content py-4">
-        <div className="container mx-auto lg:px-52 flex justify-between items-center">
-          <p className="text-sm">Copyright © 2023 - All rights reserved</p>
-          <div className="flex gap-4">
-            <Link className="">
-              <MdOutlineFacebook className="w-5 lg:w-10 h-5 lg:h-10 text-blue-500 hover:text-blue-700 transition-colors duration-300" />
-            </Link>
-            <Link className="">
-              <AiFillTwitterCircle className="w-5 lg:w-10 h-5 lg:h-10 text-blue-400 hover:text-blue-600 transition-colors duration-300" />
-            </Link>
-            <Link className="">
-              <AiFillInstagram className="w-5 lg:w-10 h-5 lg:h-10 text-pink-500 hover:text-pink-700 transition-colors duration-300" />
-            </Link>
-            <Link className="">
-              <AiFillYoutube className="w-5 lg:w-10 h-5 lg:h-10 text-red-600 hover:text-red-800 transition-colors duration-300" />
-            </Link>
+      {/* Footer for Mobile and Tablet */}
+      <footer className="text-black lg:hidden">
+        <div className="p-6">
+          <img className="w-20 h-20 rounded-full border-2 border-blue-500 mx-auto" src={logo} alt="Madrasah Logo" />
+          <p className="text-lg font-semibold text-green-500 text-center mt-4">Al-Azam High School and College</p>
+          <div className="mt-2">
+            <IoMdCall className="w-4 h-4 text-green-500 inline-block"></IoMdCall>{" "}
+            <span className="text-sm lg:text-base">01715-356338</span>
+          </div>
+          <div className="mt-2">
+            <MdEmail className="w-4 h-4 text-green-500 inline-block"></MdEmail>
+            <span className="lg:text-sm text-base">alazamhighschoolandcollege@gmail.com</span>
+          </div>
+          <div className="mt-2">
+            <MdLocationOn className="w-4 h-4 text-green-500 inline-block"></MdLocationOn>
+            <span className="lg:text-sm text-base">Amtoli, Bishwanath Upazil. Sylhet, Bangladesh</span>
           </div>
         </div>
+
+        {/* Quick Links Section */}
+        <div className="p-6">
+          <span className="footer-title text-xl block mb-4">Quick Links</span>
+          <div>
+            <div>
+              <a className="link link-hover block text-base py-1">DSHE</a>
+              <a className="link link-hover block text-base py-1">BANBEIS</a>
+              <a className="link link-hover block text-base py-1">BD National Portal</a>
+              <a className="link link-hover block text-base py-1">Ministry of Education</a>
+            </div>
+            <div>
+              <a className="link link-hover block text-base py-1">Sylhet Board</a>
+              <a className="link link-hover block text-base py-1">Primary & Mass Education</a>
+              <a className="link link-hover block text-base py-1">Form of BD. Govt</a>
+              <a className="link link-hover block text-base py-1">Pathshala EIMS</a>
+            </div>
+          </div>
+        </div>
+
+        {/* Legal Section */}
+        <div className="p-6">
+          <span className="footer-title text-xl block mb-4">Legal</span>
+          <div className="mt-2">
+            <a className="link link-hover block text-base py-1">Terms of Use</a>
+            <a className="link link-hover block text-base py-1">Privacy Policy</a>
+            <a className="link link-hover block text-base py-1">Cookie Policy</a>
+          </div>
+        </div>
+
+        {/* Social Media Section */}
+        <div className="p-6">
+          <span className="footer-title text-xl block mb-4">Follow us</span>
+          <div className="mt-2 flex justify-center items-start gap-4">
+            <div>
+              <Link to="#" className="text-blue-500 hover:text-blue-700 transition-colors duration-300">
+                <MdOutlineFacebook className="w-6 h-6 lg:w-8 lg:h-8" />
+              </Link>
+            </div>
+            <div>
+              <Link to="#" className="text-blue-400 hover:text-blue-600 transition-colors duration-300">
+                <AiFillTwitterCircle className="w-6 h-6 lg:w-8 lg:h-8" />
+              </Link>
+            </div>
+            <div>
+              <Link to="#" className="text-pink-500 hover:text-pink-700 transition-colors duration-300">
+                <AiFillInstagram className="w-6 h-6 lg:w-8 lg:h-8" />
+              </Link>
+            </div>
+            <div>
+              <Link to="#" className="text-red-600 hover:text-red-800 transition-colors duration-300">
+                <AiFillYoutube className="w-6 h-6 lg:w-8 lg:h-8" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+      {/* Copyright Section */}
+      <footer className="bg-neutral text-neutral-content py-4 text-center">
+        <p className="text-sm">Copyright © 2023 - All rights reserved</p>
       </footer>
     </div>
   );
