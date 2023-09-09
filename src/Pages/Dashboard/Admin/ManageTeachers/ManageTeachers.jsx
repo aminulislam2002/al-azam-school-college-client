724e616b1ffd5b80f640dea37f69c6bee9c8813f
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-09-09 03:07:13 -0700

    successfully load student data, show data and delete an teacher

--- src/Pages/Dashboard/Admin/ManageTeachers/ManageTeachers.jsx
+++ src/Pages/Dashboard/Admin/ManageTeachers/ManageTeachers.jsx
@@ -1,7 +1,109 @@
+import { FcViewDetails } from "react-icons/fc";
+import { MdDelete } from "react-icons/md";
+import { useQuery } from "react-query";
+import Swal from "sweetalert2";
+
 const ManageTeachers = () => {
+  const { data: teachers = [], refetch } = useQuery(["teachers"], async () => {
+    const res = await fetch("http://localhost:5000/users/teachers?role=teacher");
+    return res.json();
+  });
+
+  console.log(teachers);
+
+  const handleMakeAdmin = (user) => {
+    fetch(`http://localhost:5000/users/makeAdmin/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is an Admin Now!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
+  const handleDeleteTeacher = (_id) => {
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          Swal.fire({
+            icon: "success",
+            title: "This teacher delete Successfully",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          refetch();
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Failed to delete an teacher!",
+          });
+        }
+      });
+  };
+
   return (
     <div>
-      <h1>ManageTeachers</h1>
+      <div className="w-full lg:w-6/12 mx-auto py-3 bg-green-500">
+        <h1 className="text-lg lg:text-2xl text-center uppercase text-white">Manage all Teachers</h1>
+      </div>
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Set Role</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Row */}
+              {teachers.map((teacher, index) => (
+                <tr key={teacher._id}>
+                  <td>{index}</td>
+                  <td>{teacher.name}</td>
+                  <td>{teacher.email}</td>
+                  <td>
+                    {teacher.role === "teacher" && (
+                      <button onClick={() => handleMakeAdmin(teacher)} className="btn btn-xs btn-outline btn-primary">
+                        Make Admin
+                      </button>
+                    )}
+                  </td>
+                  <td>
+                    <div>
+                      <button>
+                        <FcViewDetails className="w-8 h-8"></FcViewDetails>
+                      </button>
+                      <button onClick={() => handleDeleteTeacher(teacher._id)}>
+                        <MdDelete className="w-8 h-8 text-red-500"></MdDelete>
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
