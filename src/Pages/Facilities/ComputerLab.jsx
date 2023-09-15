2f9488939f6fc2c2bb1dc2285f5cbd0aac922292
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-09-15 11:02:47 -0700

    update teacher section

--- src/Pages/Facilities/ComputerLab.jsx
+++ src/Pages/Facilities/ComputerLab.jsx
@@ -1,10 +1,34 @@
+import image1 from "../../assets/image/computerLab/computer-lab-1.jpg";
+import image2 from "../../assets/image/computerLab/computer-lab-2.jpg";
+import image3 from "../../assets/image/computerLab/computer-lab-3.jpg";
 
 const ComputerLab = () => {
-    return (
-        <div>
-            <h1>Data coming soon...</h1>
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <div className="container mx-auto p-6">
+        <div className="w-full lg:w-6/12 mx-auto py-3 bg-green-500 rounded-lg shadow-md">
+          <h1 className="text-3xl lg:text-4xl text-center uppercase text-white font-semibold">Our Computer Lab</h1>
         </div>
-    );
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-8">
+          <div className="lg:col-span-1">
+            <div className="bg-white rounded-lg overflow-hidden shadow-md">
+              <img src={image1} alt="Computer Lab Image 1" className="w-full h-auto" />
+            </div>
+          </div>
+          <div className="lg:col-span-1">
+            <div className="bg-white rounded-lg overflow-hidden shadow-md">
+              <img src={image2} alt="Computer Lab Image 2" className="w-full h-auto" />
+            </div>
+          </div>
+          <div className="lg:col-span-1">
+            <div className="bg-white rounded-lg overflow-hidden shadow-md">
+              <img src={image3} alt="Computer Lab Image 3" className="w-full h-auto" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ComputerLab;
--- src/Pages/Home/TeachersSection/TeachersSection.jsx
+++ src/Pages/Home/TeachersSection/TeachersSection.jsx
@@ -1,11 +1,128 @@
 import { useEffect, useState } from "react";
+import { AiOutlineMail } from "react-icons/ai";
 import { BiSolidUserRectangle } from "react-icons/bi";
 import { FaHome } from "react-icons/fa";
 import { MdCall } from "react-icons/md";
 
+import image1 from "../../../assets/image/teacher/principal.jpg";
+import image2 from "../../../assets/image/teacher/md-sharif-hossen.jpg";
+import image3 from "../../../assets/image/teacher/md-alamgir.jpg";
+import image4 from "../../../assets/image/teacher/sahin.jpg";
+
 const TeachersSection = () => {
   const [showAllTeachersData, setShowAllTeachersData] = useState(false);
   const [teachersData, setTeachersData] = useState([]);
+
+  const allTeachersDataHere = [
+    {
+      id: 1,
+      image: image1,
+      name: "এ.কে.এম আব্দুল আহাদ",
+      position: "অধ্যক্ষ",
+      mobileNumber: "+880 1715-356338",
+      email: "examplemail@gmail.com",
+      address: "বিশ্বনাথ, সিলেট",
+    },
+    {
+      id: 2,
+      image: image2,
+      name: "মো: শরীফ হোসেন",
+      position: "সহকারী শিক্ষক (ব্যবসায় শিক্ষা)",
+      mobileNumber: "+01877-686899",
+      email: " sharifhossen.info@gmail.com",
+      address: "বিশ্বনাথ, সিলেট",
+    },
+    {
+      id: 3,
+      image: image3,
+      name: "মো. আলমগীর",
+      position: "সহকারী শিক্ষক, কৃষি শিক্ষা",
+      mobileNumber: "+880 1719-351581 ",
+      email: "examplemail@gmail.com",
+      address: "বিশ্বনাথ, সিলেট",
+    },
+    {
+      id: 4,
+      image: image4,
+      name: "মো: শাহীন ইসলাম",
+      position: "কম্পিউটার ল্যাব অপারেটর",
+      mobileNumber: "+880 1575-586028",
+      email: "srdl.alazam.clo@gmail.com",
+      address: "বিশ্বনাথ, সিলেট",
+    },
+    {
+      id: 5,
+      name: "মাওঃমোঃ নাসির উদ্দিন",
+      position: "সহঃ মৌলভী",
+      mobileNumber: "০১৭৪৫৪৬৪৫১৬",
+      email: "examplemail@gmail.com",
+      address: "বিশ্বনাথ, সিলেট",
+    },
+    {
+      id: 6,
+      name: "মাঃ মোঃ রুহুল আমিন",
+      position: "সহঃ মৌলভী",
+      mobileNumber: "০১৭৫৯৬৩৮৬৯৮",
+      email: "examplemail@gmail.com",
+      address: "বিশ্বনাথ, সিলেট",
+    },
+    {
+      id: 7,
+      name: "মোঃ শহিদুল ইসলাম",
+      position: "সহঃ শিক্ষক",
+      mobileNumber: "০১৭১৪৯০৬৪৪৯",
+      email: "examplemail@gmail.com",
+      address: "বিশ্বনাথ, সিলেট",
+    },
+    {
+      id: 8,
+      name: "খালেদা আক্তার",
+      position: "I.C.T",
+      mobileNumber: "০১৭৭৭৫৩১১২১",
+      email: "examplemail@gmail.com",
+      address: "বিশ্বনাথ, সিলেট",
+    },
+    {
+      id: 9,
+      name: "আচমা আক্তার",
+      position: "সহঃ শিক্ষক",
+      mobileNumber: "০১৬৩৮৭৬০৫৩২",
+      email: "examplemail@gmail.com",
+      address: "বিশ্বনাথ, সিলেট",
+    },
+    {
+      id: 10,
+      name: "আবু সাইদ বাকি-বিল্লাহ",
+      position: "এবঃ প্রধান",
+      mobileNumber: "০১৭৯৮৫৫৬১১২",
+      email: "examplemail@gmail.com",
+      address: "বিশ্বনাথ, সিলেট",
+    },
+    {
+      id: 11,
+      name: "মোঃ মশিউর রহমান",
+      position: "জুনিয়র শিক্ষক",
+      mobileNumber: "০১৭৫৪১৬৬৮৩২",
+      email: "examplemail@gmail.com",
+      address: "বিশ্বনাথ, সিলেট",
+    },
+    {
+      id: 12,
+      name: "মোঃ আব্বাস মিয়া",
+      position: "এবঃ কারী",
+      mobileNumber: "০১৭৪৭৭৩১৪৩৪",
+      email: "examplemail@gmail.com",
+      address: "বিশ্বনাথ, সিলেট",
+    },
+    {
+      id: 13,
+      name: "মোঃ জসিম উদ্দিন",
+      position: "অফিস সহঃ",
+      mobileNumber: "",
+      email: "examplemail@gmail.com",
+      address: "বিশ্বনাথ, সিলেট",
+    },
+  ];
 
   const dataToShow = showAllTeachersData ? teachersData : teachersData.slice(0, 8);
 
@@ -18,9 +135,7 @@ const TeachersSection = () => {
   };
 
   useEffect(() => {
-    fetch("/teachers.json")
-      .then((res) => res.json())
-      .then((data) => setTeachersData(data));
+    setTeachersData(allTeachersDataHere);
   }, []);
 
   return (
@@ -35,8 +150,16 @@ const TeachersSection = () => {
             key={teacher.id}
             className="bg-blue-100 text-gray-800 hover:bg-blue-200 hover:text-gray-900 rounded-lg shadow-md p-6 transition duration-300 ease-in-out transform hover:-translate-y-1"
           >
-            <div className="bg-blue-200 rounded- p-2 mx-auto">
-              <BiSolidUserRectangle className="w-full h-40 object-cover rounded-full" />
+            <div className="bg-blue-200 rounded p-10 lg:p-2 mx-auto">
+              {teacher.image ? (
+                <>
+                  <img src={teacher?.image} className="w-full h-52" alt="Teacher" />
+                </>
+              ) : (
+                <>
+                  <BiSolidUserRectangle className="w-full h-40 object-cover rounded-full" />
+                </>
+              )}
             </div>
             <h2 className="lg:text-lg font-semibold mt-4 text-gray-900">{teacher.name}</h2>
             <p className="text-sm text-gray-600">{teacher.position}</p>
@@ -50,6 +173,12 @@ const TeachersSection = () => {
               <div className="flex items-center gap-1">
                 <MdCall className="text-green-500 text-lg" />
                 <span className="text-sm">{teacher.mobileNumber}</span>
+              </div>
+            </div>
+            <div className="mt-2">
+              <div className="flex items-center gap-1">
+                <AiOutlineMail className="text-yellow-500 text-lg" />
+                <span className="text-sm">{teacher.email}</span>
               </div>
             </div>
           </div>
